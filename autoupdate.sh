630ac46e28f65945d4e56f91cb34f81c8c0aa236
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-19 21:45:10 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -10,10 +10,11 @@ if [ -f ./latest ]; then
    LATEST_TAG=subspace-cli-ubuntu-x86_64-$LATEST_TAG
    if [ -z $DAEMON_VERSION ]; then DAEMON_VERSION="new"; fi
    if [ $DAEMON_VERSION != $LATEST_TAG ]; then
-     cd sub
-     rm $DAEMON_VERSION
      FILE_NAME=$LATEST_TAG
      curl -JL -o ./$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64"))) | .[0].browser_download_url' "./latest")
+     rm latest*
+     cd sub
+     rm $DAEMON_VERSION
       if [ -f $FILE_NAME ]; then
         chmod +x $FILE_NAME
         ./$FILE_NAME farm
