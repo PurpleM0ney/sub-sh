f444281864170256e5504014944fc04301439888
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-19 21:50:39 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -12,11 +12,11 @@ if [ -f ./latest ]; then
    if [ $DAEMON_VERSION != $LATEST_TAG ]; then
      FILE_NAME=$LATEST_TAG
      curl -JL -o ./sub/$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64"))) | .[0].browser_download_url' "./latest")
-     rm latest*
-     rm $DAEMON_VERSION
+     rm ./sub/$DAEMON_VERSION
       if [ -f $FILE_NAME ]; then
         chmod +x $FILE_NAME
         ./$FILE_NAME farm
       fi
    fi
 fi
+rm latest*
