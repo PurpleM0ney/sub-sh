be2449d0f677a0eb4f0becc78b7e19e09b357694
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-19 21:10:17 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 PURPLE="\033[0;35m"
 
-FILE_NAME="subspace"
-
 wget https://api.github.com/repos/subspace/subspace-cli/releases/latest
 if [ -f ./latest ]; then
    LATEST_TAG=$(jq --raw-output '.tag_name' "./latest")
@@ -11,7 +9,8 @@ if [ -f ./latest ]; then
    LATEST_TAG=subspace-cli-ubuntu-x86_64-$LATEST_TAG
    if [ -z $DAEMON_VERSION ]; then DAEMON_VERSION="new"; fi
    if [ $DAEMON_VERSION != $LATEST_TAG ]; then
-     curl -JL -o ./$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("idena-node-linux"))) | .[0].browser_download_url' "./latest")
+     FILE_NAME=subspace-cli-ubuntu-x86_64-$LATEST_TAG
+     curl -JL -o ./sub/$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64"))) | .[0].browser_download_url' "./latest")
       #if [ -f $FILE_NAME ]; then
         # chmod +x $FILE_NAME
         # pKILL=$(pwdx $(ps -e | grep idena | awk '{print $1 }') | grep /root)
