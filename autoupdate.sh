1ac0e99b9e7aef556502d5bd4cf254e96a9bb0cd
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-19 21:41:14 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -6,6 +6,7 @@ if [ -f ./latest ]; then
    LATEST_TAG=$(jq --raw-output '.tag_name' "./latest")
    #LATEST_TAG=${LATEST_TAG//v/}
    DAEMON_VERSION=$(ls ~/subspace-sh/sub/)
+   echo $DAEMON_VERSION
    LATEST_TAG=subspace-cli-ubuntu-x86_64-$LATEST_TAG
    if [ -z $DAEMON_VERSION ]; then DAEMON_VERSION="new"; fi
    if [ $DAEMON_VERSION != $LATEST_TAG ]; then
