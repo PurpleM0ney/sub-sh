e12148062313e51e9700c47c3d7d1f76e2d0844b
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-20 01:14:38 +0300

    Update install_update.sh

--- install_update.sh
+++ install_update.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-#names
-SCRIPT_NAME="autoupdate.sh"
-SCRIPT_PATH="subspace-scripts"
-
 #color
 GREEN="\033[0;32m"
 DEFAULT="\033[0m"
-
-if [[ "$USER" == "root" ]]; then
-        HOMEFOLDER="/root"
-else
-        HOMEFOLDER="/home/$USER"
-fi
-
-CURRENTDIR=$(pwd)
-if [ ! -d $HOMEFOLDER/$SCRIPT_PATH ]; then mkdir $HOMEFOLDER/$SCRIPT_PATH; fi
-cd $HOMEFOLDER/$SCRIPT_PATH
-
-echo "Start install autoupdate scripts"
-#Доделай
 
 wget https://api.github.com/repos/subspace/subspace-cli/releases/latest
 if [ -f ./latest ]; then
@@ -30,13 +13,10 @@ if [ -f ./latest ]; then
    if [ $DAEMON_VERSION != $LATEST_TAG ]; then
      FILE_NAME=$LATEST_TAG
      curl -JL -o ./sub/$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64"))) | .[0].browser_download_url' "./latest")
-      if [ $DAEMON_VERSION != "new" ]; then rm ./sub/$DAEMON_VERSION; fi
+     rm ./sub/$DAEMON_VERSION
       if [ -f ./sub/$FILE_NAME ]; then
         chmod +x ./sub/$FILE_NAME
-        echo ""
-        CUR_VER=$FILE_NAME
-        CUR_VER=${CUR_VER//subspace-cli-ubuntu-x86_64-v/}
-        echo -e "${GREEN}Version $CUR_VER successfully installed"
+        echo -e "${GREEN}Version $FILE_NAME successfully installed"
         echo -e "\033[0m"
       fi
    fi
