4d366670ae13cdae771a0a38103bf62d39dfd2b9
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-19 21:03:28 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -8,10 +8,11 @@ if [ -f ./latest ]; then
    LATEST_TAG=$(jq --raw-output '.tag_name' "./latest")
    #LATEST_TAG=${LATEST_TAG//v/}
    DAEMON_VERSION=$(ls ~/subspace-sh/sub/)
-   LATEST_TAG=subspace-cli-ubuntu-x86_64-+$LATEST_TAG
+   LATEST_TAG=subspace-cli-ubuntu-x86_64-$LATEST_TAG
    echo $LATEST_TAG
+   echo $DAEMON_VERSION
   # [ -z $DAEMON_VERSION ]; then DAEMON_VERSION="new"; fi
-  # if [ $DAEMON_VERSION != subspace-cli-ubuntu-x86_64-$LATEST_TAG ]; then
+   #if [ $DAEMON_VERSION != $LATEST_TAG ]; then
      # curl -JL -o ./$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("idena-node-linux"))) | .[0].browser_download_url' "./latest")
       #if [ -f $FILE_NAME ]; then
         # chmod +x $FILE_NAME
