198295b5239b4866bc6c14054966d82830fc78b1
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-19 20:20:28 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,50 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-SCRIPT_NAME="subspaceupdate.sh"
-DAEMON_FILE="subspace"
-SCRIPT_PATH="subspace-scripts"
-DAEMON_PATH="subspace"
+FILE_NAME="subspace"
 
-if [[ "$USER" == "root" ]]; then
-        HOMEFOLDER="/root"
-else
-        HOMEFOLDER="/home/$USER"
+wget https://api.github.com/repos/idena-network/idena-go/releases/latest
+if [ -f ./latest ]; then
+   LATEST_TAG=$(jq --raw-output '.tag_name' "./latest")
+   LATEST_TAG=${LATEST_TAG//v/}
+   DAEMON_VERSION=$(/root/idena/idena-go -v | awk '{print $3}')
+   if [ -z $DAEMON_VERSION ]; then DAEMON_VERSION="new"; fi
+   if [ $DAEMON_VERSION != $LATEST_TAG ]; then
+      curl -JL -o ./$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("idena-node-linux"))) | .[0].browser_download_url' "./latest")
+      if [ -f $FILE_NAME ]; then
+         chmod +x $FILE_NAME
+         pKILL=$(pwdx $(ps -e | grep idena | awk '{print $1 }') | grep /root)
+         pKILL=$(echo $pKILL | awk '{print $1}' | sed s/.$//)
+         if [ ! -z pKILL ]; then systemctl stop idena.service; fi
+         mv $FILE_NAME /root/idena/idena-go
+         systemctl start idena.service
+      fi
+   fi
 fi
-SERVICE_NAME="subspace"
-
-CURRENTDIR=$(pwd)
-if [ ! -d $HOMEFOLDER/$SCRIPT_PATH ]; then mkdir $HOMEFOLDER/$SCRIPT_PATH; fi
-cd $HOMEFOLDER/$SCRIPT_PATH
-
-echo "Create script file..."
-
-echo "#!/bin/bash" > $SCRIPT_NAME
-echo >> $SCRIPT_NAME
-echo 'FILE_NAME="subspace"' >> $SCRIPT_NAME
-echo >> $SCRIPT_NAME
-echo 'wget https://api.github.com/repos/subspace/subspace-cli/releases/latest' >> $SCRIPT_NAME
-echo 'if [ -f ./latest ]; then' >> $SCRIPT_NAME
-echo '   LATEST_TAG=$(jq --raw-output '"'"'.tag_name'"'"' "./latest")' >> $SCRIPT_NAME
-echo '   LATEST_TAG=${LATEST_TAG//v/}' >> $SCRIPT_NAME
-echo -n '   DAEMON_VERSION=$(' >> $SCRIPT_NAME
-echo -n -e "$HOMEFOLDER/$DAEMON_PATH/$DAEMON_FILE -v | awk " >> $SCRIPT_NAME
-echo ''\''{print $3}'\'')' >> $SCRIPT_NAME
-echo '   if [ -z $DAEMON_VERSION ]; then DAEMON_VERSION="new"; fi' >> $SCRIPT_NAME
-echo '   if [ $DAEMON_VERSION != $LATEST_TAG ]; then' >> $SCRIPT_NAME
-echo -n '      curl -JL -o ./$FILE_NAME $' >> $SCRIPT_NAME
-echo '(jq --raw-output '"'"'.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64-"))) | .[0].browser_download_url'"'"' "./latest")' >> $SCRIPT_NAME
-echo '      if [ -f $FILE_NAME ]; then' >> $SCRIPT_NAME
-echo '         chmod +x $FILE_NAME' >> $SCRIPT_NAME
-echo '         pKILL=$(pwdx $(ps -e | grep subspace | awk '"'"'{print $1 }'"'"') | grep /root)' >> $SCRIPT_NAME
-echo '         pKILL=$(echo $pKILL | awk '"'"'{print $1}'"'"' | sed 's/.$//')' >> $SCRIPT_NAME
-echo -n '         if [ ! -z pKILL ]; then systemctl ' >> $SCRIPT_NAME
-echo -e "stop $SERVICE_NAME.service; fi" >> $SCRIPT_NAME
-echo -n '         mv $FILE_NAME ' >> $SCRIPT_NAME
-echo -e "$HOMEFOLDER/$DAEMON_PATH/$DAEMON_FILE" >> $SCRIPT_NAME
-echo -e "         systemctl start $SERVICE_NAME.service" >> $SCRIPT_NAME
-echo '      fi' >> $SCRIPT_NAME
-echo '   fi' >> $SCRIPT_NAME
-echo 'fi' >> $SCRIPT_NAME
-echo 'rm latest*' >> $SCRIPT_NAME
-chmod +x $SCRIPT_NAME
-cd $CURRENTDIR
+rm latest*
