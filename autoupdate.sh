df5d30a8d6479f78b64a28e4626fc9fb25052c63
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-20 00:13:29 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -5,7 +5,7 @@ wget https://api.github.com/repos/subspace/subspace-cli/releases/latest
 if [ -f ./latest ]; then
    LATEST_TAG=$(jq --raw-output '.tag_name' "./latest")
    DAEMON_VERSION=$(ls ~/subspace-sh/sub/)
-   echo "${PURPLE} Текущая версия ноды - $DAEMON_VERSION"
+   echo "${PURPLE}Текущая версия ноды - $DAEMON_VERSION"
    LATEST_TAG=subspace-cli-ubuntu-x86_64-$LATEST_TAG
    if [ -z $DAEMON_VERSION ]; then DAEMON_VERSION="new"; fi
    if [ $DAEMON_VERSION != $LATEST_TAG ]; then
@@ -14,7 +14,7 @@ if [ -f ./latest ]; then
      rm ./sub/$DAEMON_VERSION
       if [ -f ./sub/$FILE_NAME ]; then
         chmod +x ./sub/$FILE_NAME
-        echo "${PURPLE} Версия $DAEMON_VERSION установлена, запускаем фармер"
+        echo "${PURPLE}Версия $DAEMON_VERSION установлена, запускаем фармер"
         ./sub/./$FILE_NAME farm
       fi
    fi
