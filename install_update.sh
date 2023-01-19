c33f7e520ba8b65697f8c470fbe9a5db8caa52f3
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-20 00:59:53 +0300

    Update install_update.sh

--- install_update.sh
+++ install_update.sh
@@ -43,7 +43,8 @@ if [ -f ./latest ]; then
 fi
 
 rm latest*
-if [ -z $FILE_NAME ]; then 
-./sub/./$DAEMON_VERSION farm 
-else ./sub/./$FILE_NAME farm 
+if [! -z $FILE_NAME ]; then 
+        ./sub/./$DAEMON_VERSION farm 
+else 
+        ./sub/./$FILE_NAME farm 
 fi
