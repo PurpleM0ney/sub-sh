0e4113dd0a1c6bcab633edb1a0485ac5f0464d24
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-19 21:02:13 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -7,13 +7,12 @@ wget https://api.github.com/repos/subspace/subspace-cli/releases/latest
 if [ -f ./latest ]; then
    LATEST_TAG=$(jq --raw-output '.tag_name' "./latest")
    #LATEST_TAG=${LATEST_TAG//v/}
+   DAEMON_VERSION=$(ls ~/subspace-sh/sub/)
+   LATEST_TAG=subspace-cli-ubuntu-x86_64-+$LATEST_TAG
    echo $LATEST_TAG
-   NAME_FILE=$(ls ~/subspace-sh/sub/)
-   echo $NAME_FILE
-   #DAEMON_VERSION=$(./subspace-sh/sub/')
-   #if [ -z $DAEMON_VERSION ]; then DAEMON_VERSION="new"; fi
-   #if [ $DAEMON_VERSION != $LATEST_TAG ]; then
-      #curl -JL -o ./$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("idena-node-linux"))) | .[0].browser_download_url' "./latest")
+  # [ -z $DAEMON_VERSION ]; then DAEMON_VERSION="new"; fi
+  # if [ $DAEMON_VERSION != subspace-cli-ubuntu-x86_64-$LATEST_TAG ]; then
+     # curl -JL -o ./$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("idena-node-linux"))) | .[0].browser_download_url' "./latest")
       #if [ -f $FILE_NAME ]; then
         # chmod +x $FILE_NAME
         # pKILL=$(pwdx $(ps -e | grep idena | awk '{print $1 }') | grep /root)
