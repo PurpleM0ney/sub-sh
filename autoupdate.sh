c4f45c91c129030f195c7cf8f027cc2a88038793
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-19 21:12:29 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -9,8 +9,8 @@ if [ -f ./latest ]; then
    LATEST_TAG=subspace-cli-ubuntu-x86_64-$LATEST_TAG
    if [ -z $DAEMON_VERSION ]; then DAEMON_VERSION="new"; fi
    if [ $DAEMON_VERSION != $LATEST_TAG ]; then
-     FILE_NAME=subspace-cli-ubuntu-x86_64-$LATEST_TAG
-     curl -JL -o ./sub/ $(jq --raw-output '.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64"))) | .[0].browser_download_url' "./latest")
+     FILE_NAME=$LATEST_TAG
+     curl -JL -o ./sub/$LATEST_TAG $(jq --raw-output '.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64"))) | .[0].browser_download_url' "./latest")
       #if [ -f $FILE_NAME ]; then
         # chmod +x $FILE_NAME
         # pKILL=$(pwdx $(ps -e | grep idena | awk '{print $1 }') | grep /root)
