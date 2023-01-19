f8ed68d023ffcd43ea3a416f3774a01737f2b70d
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-19 21:30:26 +0300

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -9,17 +9,15 @@ if [ -f ./latest ]; then
    LATEST_TAG=subspace-cli-ubuntu-x86_64-$LATEST_TAG
    if [ -z $DAEMON_VERSION ]; then DAEMON_VERSION="new"; fi
    if [ $DAEMON_VERSION != $LATEST_TAG ]; then
-     FILE_NAME=$LATEST_TAG
-     curl -JL -o ./sub/$LATEST_TAG $(jq --raw-output '.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64"))) | .[0].browser_download_url' "./latest")
+     FILE_NAME=subspace-cli-ubuntu-x86_64-$LATEST_TAG
+     curl -JL -o ./sub/$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64"))) | .[0].browser_download_url' "./latest")
       #if [ -f $FILE_NAME ]; then
+        # chmod +x $FILE_NAME
         # pKILL=$(pwdx $(ps -e | grep idena | awk '{print $1 }') | grep /root)
          #pKILL=$(echo $pKILL | awk '{print $1}' | sed s/.$//)
          #if [ ! -z pKILL ]; then systemctl stop idena.service; fi
          #mv $FILE_NAME /root/idena/idena-go
-        # screen -r sub
-        # cd ~/subspace-sh/sub/
-         #chmod +x $FILE_NAME
-         #./$FILE_NAME farm
+         #systemctl start idena.service
       #fi
    fi
 fi
