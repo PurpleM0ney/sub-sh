82ce2de407ca946d288b27aefd2135fa4f5ab4c2
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-20 17:32:02 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -7,12 +7,15 @@ DEFAULT="\033[0m"
 wget https://api.github.com/repos/subspace/subspace-cli/releases
 if [ -f ./releases ]; then
    LATEST_TAG=$(curl https://api.github.com/repos/subspace/subspace/releases | jq '[.[] | select(.prerelease==true) | select(.tag_name | startswith("runtime") | not) | select(.tag_name | startswith("chain-spec") | not)][0].tag_name')
-   echo $LATEST_TAG
    
    VERSION_NODE=$(ls ~/subspace-sh/sub/ | grep node)
    VERSION_FARMER=$(ls ~/subspace-sh/sub/ | grep farmer)
-   LATEST_TAG=subspace-cli-ubuntu-x86_64-$LATEST_TAG
+   
+   LATEST_NODE=subspace-farmer-ubuntu-x86_64-$LATEST_TAG
+   LATEST_FARMER=subspace-cli-ubuntu-x86_64-$LATEST_TAG
    
    echo $VERSION_FARMER
    echo $VERSION_NODE
+   echo "Farmer $LATEST_FARMER"
+   echo "Node $LATEST_NODE"
 fi
