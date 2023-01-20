279483b73e8d398deececd4c0759e3cf002de60f
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-20 19:45:24 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -4,8 +4,8 @@
 GREEN="\033[0;32m"
 DEFAULT="\033[0m"
 
-#wget https://api.github.com/repos/subspace/subspace-cli/releases
-if [ -f ./releases ]; then
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
    LATEST_TAG=$(curl https://api.github.com/repos/subspace/subspace/releases | jq --raw-output '[.[] | select(.prerelease==true) | select(.tag_name | startswith("runtime") | not) | select(.tag_name | startswith("chain-spec") | not)][0].tag_name')
    echo $LATEST_TAG
    
@@ -18,14 +18,23 @@ if [ -f ./releases ]; then
    LATEST_FARMER=subspace-cli-ubuntu-x86_64-$LATEST_TAG
    
    if [ -z $VERSION_NODE ]; then 
-   wget -P ~/subspace-sh/sub https://github.com/subspace/subspace/releases/download/$LATEST_TAG/subspace-node-ubuntu-x86_64-$LATEST_TAG
-   screen -S subNode
-   sleep 2
-   echo "АЛЛО"
+   cd $HOME
+   wget  https://github.com/subspace/subspace/releases/download/$LATEST_TAG/subspace-node-ubuntu-x86_64-$LATEST_TAG
+   chmod +x subspace*
+   mv subspace* /usr/local/bin/
+
+   source ~/.bash_profile
+sleep 1
    fi
    
    if [ -z $VERSION_FARMER ]; then 
-   wget -P ~/subspace-sh/sub https://github.com/subspace/subspace/releases/download/$LATEST_TAG/subspace-farmer-ubuntu-x86_64-$LATEST_TAG
+   cd $HOME
+   wget https://github.com/subspace/subspace/releases/download/$LATEST_TAG/subspace-farmer-ubuntu-x86_64-$LATEST_TAG
+   chmod +x subspace*
+   mv subspace* /usr/local/bin/
+
+   source ~/.bash_profile
+ sleep 1
    fi
    
    #Отладка
