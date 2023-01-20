a9dd804e050070fefb34303ca81ca0cefa66fe02
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-20 17:57:47 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -18,7 +18,7 @@ if [ -f ./releases ]; then
    
    if [ -z $VERSION_NODE ]; then 
    NODE_NAME=$VERSION_NODE
-   curl -JL -o ./sub/$NODE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("subspace-node-ubuntu-x86_64"))) | .[0].browser_download_url' "./releases")
+   curl -JL -o ./sub/$NODE_NAME $(curl https://api.github.com/repos/subspace/subspace/releases | jq --raw-output '[.[] | select(.prerelease==true) | select(.tag_name | startswith("subspace-node-ubuntu-x86_64") | not) | select(.tag_name | startswith("chain-spec") | not)[0].assets[].browser_download_url")
    fi
    
    if [ -z $VERSION_FARMER ]; then echo "Установи фармер"; fi
