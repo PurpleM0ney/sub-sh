277bc3d70ab2c23b0859bfd4c1bd9d080904acdc
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-20 18:04:51 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -18,7 +18,7 @@ if [ -f ./releases ]; then
    
    if [ -z $VERSION_NODE ]; then 
    NODE_NAME=$VERSION_NODE
-   im=$(curl https://api.github.com/repos/subspace/subspace/releases | jq --raw-output '[.[] | select(.prerelease==true) | map(select(.name | startswith("subspace-node-ubuntu-x86_64"))) | not) | select(.tag_name | startswith("chain-spec") | not)][0].assets[].browser_download_url')
+   #im=$(curl https://api.github.com/repos/subspace/subspace/releases | jq --raw-output '[.[] | select(.prerelease==true) | map(select(.name | startswith("subspace-node-ubuntu-x86_64"))) | not) | select(.tag_name | startswith("chain-spec") | not)][0].assets[].browser_download_url')
    echo $im
    fi
    
