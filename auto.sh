cabf92a04d0cef60e47165815a5a882437317e43
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-20 17:16:19 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -3,8 +3,9 @@
 #color
 GREEN="\033[0;32m"
 DEFAULT="\033[0m"
+
 wget https://api.github.com/repos/subspace/subspace-cli/releases
 if [ -f ./releases ]; then
-   LATEST_TAG=$(jq '[.[] | select(.prerelease==false) | select(.tag_name | startswith("runtime")|not)][0]')
+   LATEST_TAG=$(curl https://api.github.com/repos/subspace/subspace/releases | jq '[.[] | select(.prerelease==false) | select(.tag_name | startswith("runtime") | not) | select(.tag_name | startswith("chain-spec") | not)][0].assets[].browser_download_url')
    echo $LATEST_TAG
 fi
