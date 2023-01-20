0e85647decd7488b6a244a5a720271f41b866fed
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-20 18:24:38 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -6,7 +6,7 @@ DEFAULT="\033[0m"
 
 #wget https://api.github.com/repos/subspace/subspace-cli/releases
 if [ -f ./releases ]; then
-   LATEST_TAG=$(jq --raw-output '[.[] | select(.prerelease==true) | select(.tag_name | startswith("runtime") | not) | select(.tag_name | startswith("chain-spec") | not)][0].tag_name' "./releases")
+   LATEST_TAG=$(curl https://api.github.com/repos/subspace/subspace/releases | jq --raw-output '[.[] | select(.prerelease==true) | select(.tag_name | startswith("runtime") | not) | select(.tag_name | startswith("chain-spec") | not)][0].tag_name')
    echo $LATEST_TAG
    #Получаем какие версии у нас на ноде
    VERSION_NODE=$(ls ~/subspace-sh/sub/ | grep node)
@@ -18,7 +18,7 @@ if [ -f ./releases ]; then
    
    if [ -z $VERSION_NODE ]; then 
    NODE_NAME=$VERSION_NODE
-   im=$(jq '[.[] | select(.prerelease==true) | select(.tag_name | startswith("subspace-node-ubuntu-x86_64") | not) | select(.tag_name | startswith("chain-spec") | not)][0].assets[].browser_download_url' "./releases")
+   im=$(curl https://api.github.com/repos/subspace/subspace/releases | jq '[.[] | select(.prerelease==true) | select(.tag_name | startswith("subspace-node-ubuntu-x86_64") | not) | select(.tag_name | startswith("chain-spec") | not)][0].assets[].browser_download_url')
    echo $im
    fi
    
