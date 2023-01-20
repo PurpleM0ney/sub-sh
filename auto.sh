a3d4f671bea09c183583d90d473889e9f77e7cff
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-20 18:22:45 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -7,7 +7,7 @@ DEFAULT="\033[0m"
 #wget https://api.github.com/repos/subspace/subspace-cli/releases
 if [ -f ./releases ]; then
    LATEST_TAG=$(jq --raw-output '[.[] | select(.prerelease==true) | select(.tag_name | startswith("runtime") | not) | select(.tag_name | startswith("chain-spec") | not)][0].tag_name' "./releases")
-   
+   echo $LATEST_TAG
    #Получаем какие версии у нас на ноде
    VERSION_NODE=$(ls ~/subspace-sh/sub/ | grep node)
    VERSION_FARMER=$(ls ~/subspace-sh/sub/ | grep farmer)
