6e33d8d235fb22948a57e93b0f30c6d61efdfc9c
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-20 20:04:54 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -8,6 +8,7 @@ bash_profile=$HOME/.bash_profile
 if [ -f "$bash_profile" ]; then
    LATEST_TAG=$(curl https://api.github.com/repos/subspace/subspace/releases | jq --raw-output '[.[] | select(.prerelease==true) | select(.tag_name | startswith("runtime") | not) | select(.tag_name | startswith("chain-spec") | not)][0].tag_name')
    
+   #Создаём папку для ноды
    DIR=$(ls ~/ | grep subspace)
    if [ -z $DIR ]; then mkdir /root/subspace; fi
    
@@ -19,25 +20,25 @@ if [ -f "$bash_profile" ]; then
    LATEST_NODE=subspace-farmer-ubuntu-x86_64-$LATEST_TAG
    LATEST_FARMER=subspace-cli-ubuntu-x86_64-$LATEST_TAG
    
+   #Проверяем наличие ноды, если нет - то качаем
    if [ -z $VERSION_NODE ]; then 
    cd $HOME
    wget  https://github.com/subspace/subspace/releases/download/$LATEST_TAG/subspace-node-ubuntu-x86_64-$LATEST_TAG
    chmod +x subspace*
    mv subspace-node-ubuntu-x86_64-$LATEST_TAG /root/subspace/
-
-   source ~/.bash_profile
-sleep 1
    fi
    
+   #Проверяем наличие фармера, если нет - то качаем
    if [ -z $VERSION_FARMER ]; then 
    cd $HOME
    wget https://github.com/subspace/subspace/releases/download/$LATEST_TAG/subspace-farmer-ubuntu-x86_64-$LATEST_TAG
    chmod +x subspace*
    mv subspace-farmer-ubuntu-x86_64-$LATEST_TAG /root/subspace/
-
-   source ~/.bash_profile
- sleep 1
    fi
+   
+   
+   source ~/.bash_profile
+   sleep 1
    
    #Отладка
    VERSION_NODE=$(ls ~/subspace/ | grep node)
