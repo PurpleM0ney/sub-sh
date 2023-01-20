90f10942445a06e1f40acfec0bfe947ffc6e7e12
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-20 18:38:17 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -8,18 +8,18 @@ DEFAULT="\033[0m"
 if [ -f ./releases ]; then
    LATEST_TAG=$(curl https://api.github.com/repos/subspace/subspace/releases | jq --raw-output '[.[] | select(.prerelease==true) | select(.tag_name | startswith("runtime") | not) | select(.tag_name | startswith("chain-spec") | not)][0].tag_name')
    echo $LATEST_TAG
+   
    #Получаем какие версии у нас на ноде
    VERSION_NODE=$(ls ~/subspace-sh/sub/ | grep node)
    VERSION_FARMER=$(ls ~/subspace-sh/sub/ | grep farmer)
-   
+
    #Получаем версию, которая должна быть
    LATEST_NODE=subspace-farmer-ubuntu-x86_64-$LATEST_TAG
    LATEST_FARMER=subspace-cli-ubuntu-x86_64-$LATEST_TAG
    
    if [ -z $VERSION_NODE ]; then 
    NODE_NAME=$VERSION_NODE
-   im=$(curl https://api.github.com/repos/subspace/subspace/releases | jq '[.[] | select(.prerelease==true) | map(select(.name | startswith("subspace-cli-ubuntu-x86_64")))][0].assets[].browser_download_url')
-   echo $im
+   wget https://github.com/subspace/subspace/releases/download/$LATEST_TAG/subspace-farmer-ubuntu-x86_64-$LATEST_TAG
    fi
    
    if [ -z $VERSION_FARMER ]; then echo "Установи фармер"; fi
