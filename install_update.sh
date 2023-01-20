d7f7cf9ab204db41ae657758b184bee2b64526a0
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-20 12:32:44 +0300

    Update install_update.sh

--- install_update.sh
+++ install_update.sh
@@ -10,18 +10,7 @@ if [ -f ./latest ]; then
    DAEMON_VERSION=$(ls ~/subspace-sh/sub/)
    LATEST_TAG=subspace-cli-ubuntu-x86_64-$LATEST_TAG
    
-   if [[ -z $DAEMON_VERSION ]]; then
-    FILE_NAME=$LATEST_TAG
-    curl -JL -o ./sub/$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64"))) | .[0].browser_download_url' "./latest")
-    chmod +x ./sub/$FILE_NAME
-    CUR_VER=${FILE_NAME//subspace-cli-ubuntu-x86_64-/}
-    echo ""
-    echo -e "${GREEN}The node has been successfully installed! The current version is $CUR_VER"
-    echo -e "\033[0m"
-    rm latest*
-    ./sub/./$FILE_NAME init
-    ./sub/./$FILE_NAME farm
-   fi
+   if [[ -z $DAEMON_VERSION ]]; then DAEMON_VERSION="new"; fi
    
    if [[ $DAEMON_VERSION != $LATEST_TAG ]]; then
      FILE_NAME=$LATEST_TAG
