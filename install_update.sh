65541e09578a154a9a65a741a7c249145f8bef14
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-21 15:20:35 +0300

    Update install_update.sh

--- install_update.sh
+++ install_update.sh
@@ -14,8 +14,8 @@ if [ -f ./latest ]; then
    curl -JL -o ./sub/$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64"))) | .[0].browser_download_url' "./latest")
    chmod +x ./sub/$FILE_NAME
    rm latest*
-   exec ./sub/./$FILE_NAME init
-   echo "443gfdcgfd"
+   screen -S subInit
+   screen -r demo -X stuff  "'/root/sub-sh/sub/./$FILE_NAME init^M'"
    sleep 1
    CUR_VER=${FILE_NAME//subspace-cli-ubuntu-x86_64-/}
    echo "-----------------------------------------------------------------------------"
