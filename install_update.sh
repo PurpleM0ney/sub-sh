587ac4ff2c0073bcc6f10b82c771b32b757f33a4
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-23 18:03:41 +0300

    Update install_update.sh

--- install_update.sh
+++ install_update.sh
@@ -48,7 +48,7 @@ if [ -f ./latest ]; then
      BODY=$(jq '.body' "./latest")
      BODY=${BODY//before starting*/}
      BODY=${BODY//*you should/}
-     BODY='wipe'
+     BODY=${BODY// */}
      echo $BODY
    if [ "$BODY" = "wipe" ]; then
      echo "WIPEEEEEEEEE"
