bfe1fca42e4c3249057b9686e87d59f32eed1fb2
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-23 17:41:04 +0300

    Update install_update.sh

--- install_update.sh
+++ install_update.sh
@@ -6,7 +6,7 @@ DEFAULT="\033[0m"
 
 wget https://api.github.com/repos/subspace/subspace-cli/releases/latest
    BODY=$(jq '.body' "./latest")
-   BODY=${BODY//you should wipe/}
+   BODY=${v%you*}
    echo $BODY
 if [ -f ./latest ]; then
    LATEST_TAG=$(jq --raw-output '.tag_name' "./latest")
