0b3e67e0e8cd552731e5efd034057faa23a0499a
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-25 18:11:20 +0300

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -32,8 +32,9 @@ fi
 #------------------- Блок с проверкой установки ноды и ее установкой (если не найдена) ----------------------------
 
 wget -P ~/SubSpace/ https://api.github.com/repos/subspace/subspace-cli/releases/latest
-if [ -f ./latest ]; then
-   LATEST_TAG=$(jq --raw-output '.tag_name' "./latest")
+if [ -f ~/SubSpace/./latest ]; then
+   LATEST_TAG=$(jq --raw-output '.tag_name' "~/SubSpace/./latest")
+   echo $LATEST_TAG
    DAEMON_VERSION=$(ls ~/SubSpace/NODE/)
    LATEST_TAG=subspace-cli-ubuntu-x86_64-$LATEST_TAG
    
