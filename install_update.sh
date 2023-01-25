31c771c6e35c87dc8bd07f27e954319a391da1a3
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-25 14:33:24 +0300

    Update install_update.sh

--- install_update.sh
+++ install_update.sh
@@ -34,8 +34,8 @@ if [ -f ./latest ]; then
    #Нода не устанолвена
    if [ -z $DAEMON_VERSION ]; then
    FILE_NAME=$LATEST_TAG
-   curl -JL -o ./SubSpace/$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64"))) | .[0].browser_download_url' "./latest")
-   chmod +x ./SubSpace/$FILE_NAME
+   curl -JL -o ~/SubSpace/$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64"))) | .[0].browser_download_url' "./latest")
+   chmod +x ~/SubSpace/$FILE_NAME
    
    #создаем screen Init
    screen -d -m -S subInit
