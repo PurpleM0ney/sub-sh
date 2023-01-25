91180a02733215010e272045a2895aff3a42829b
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-25 16:06:51 +0300

    Update install_update_v1.sh

--- install_update_v1.sh
+++ install_update_v1.sh
@@ -77,70 +77,72 @@ if [ -f ./latest ]; then
    fi
    
 #------------------- Блок с проверкой на наличие новых версий и последующим обновлением ----------------------------
-  # if [[ $DAEMON_VERSION != $LATEST_TAG ]]; then
+   if [[ $DAEMON_VERSION != $LATEST_TAG ]]; then
     
-  #   FILE_NAME=$LATEST_TAG
-  #   screen -X -S subFarm quit
+      FILE_NAME=$LATEST_TAG
+      screen -X -S subFarm quit
       
      #Получаем описание обновления
-  #   BODY=$(jq '.body' "./latest")
-  #   BODY=${BODY//before starting*/}
-  #   BODY=${BODY//*you should/}
+     BODY=$(jq '.body' "./latest")
+     BODY=${BODY//before starting*/}
+     BODY=${BODY//*you should/}
      
-  # if [[ ! -z $BODY ]]; then
-  #   sleep 1
+   if [[ ! -z $BODY ]]; then
+     sleep 1
      #./sub/./$DAEMON_VERSION wipe
-  #   echo "-----------------------------------------------------------------------------"
-  #   echo -e "\n\e[42mWipe successful!\e[0m\n"
-  #   echo "-----------------------------------------------------------------------------" 
-  # fi
+     echo "-----------------------------------------------------------------------------"
+     echo -e "\n\e[42mWipe successful!\e[0m\n"
+     echo "-----------------------------------------------------------------------------" 
+   fi
      
-     #Выполняем обновление
-   #  curl -JL -o ./sub/$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64"))) | .[0].browser_download_url' "./latest")
-  #   rm ./sub/$DAEMON_VERSION
-   #   if [ -f ./sub/$FILE_NAME ]; then
-  #      chmod +x ./sub/$FILE_NAME
-   #     CUR_VER=${FILE_NAME//subspace-cli-ubuntu-x86_64-/}
-   #     
-        #создаем screen для Init
-   #     screen -d -m -S subInit
-   #     screen -r subInit -X stuff  "/root/subspace-sh/sub/./$FILE_NAME init^M"
-   #     sleep 2
-     #   screen -r subInit -X stuff  "$WALLET"
-    #    sleep 2
-     #   screen -r subInit -X stuff  "^M"
-    #    sleep 2
-    #    screen -r subInit -X stuff  "$NAME^M"
-    #    sleep 1
-     #   screen -r subInit -X stuff  "^M"
-     #   sleep 1
-     #   screen -r subInit -X stuff  "^M"
-    #    sleep 1
-    #    screen -r subInit -X stuff  "^M" 
-    #    sleep 1
-    #   screen -X -S subInit quit
-    #    sleep 1
-        
-        #Создаем screen Farm
-     #   screen -d -m -S subFarm
-    #   sleep 1
-     #   screen -r subFarm -X stuff  "/root/subspace-sh/sub/./$FILE_NAME farm^M"
-    #    sleep 1
+   #Выполняем обновление
+   curl -JL -o ./sub/$FILE_NAME $(jq --raw-output '.assets | map(select(.name | startswith("subspace-cli-ubuntu-x86_64"))) | .[0].browser_download_url' "./latest")
+   rm ~/SubSpace/NODE/$DAEMON_VERSION
    
-    #    echo "-----------------------------------------------------------------------------"
-    #    echo -e "\n\e[42mThe node has been successfully updated! The current version is $CUR_VER\e[0m\n"
-    #    echo -e "You can check the operation of farmer with the command: \n\e[31mscreen -r subFarm\e[0m\n"
-    #    echo "-----------------------------------------------------------------------------"
-   #     rm latest*
-   #   fi
+   if [ -f ~/SubSpace/NODE/$FILE_NAME ]; then
+      mv $FILE_NAME ~/SubSpace/NODE/
+      chmod +x ~/SubSpace/NODE/$FILE_NAME
+      CUR_VER=${FILE_NAME//subspace-cli-ubuntu-x86_64-/}
+        
+      #Cоздаем screen для Init
+      screen -d -m -S subInit
+      screen -r subInit -X stuff  "~/SubSpace/NODE/./$FILE_NAME init^M"
+      sleep 2
+      screen -r subInit -X stuff  "$WALLET"
+      sleep 2
+      screen -r subInit -X stuff  "^M"
+      sleep 2
+      screen -r subInit -X stuff  "$NAME^M"
+      sleep 1
+      screen -r subInit -X stuff  "^M"
+      sleep 1
+      screen -r subInit -X stuff  "^M"
+      sleep 1
+      screen -r subInit -X stuff  "^M" 
+      sleep 1
+      screen -X -S subInit quit
+      sleep 1
+        
+      #Создаем screen Farm
+      screen -d -m -S subFarm
+      sleep 1
+      screen -r subFarm -X stuff  "~/SubSpace/NODE/./$FILE_NAME farm^M"
+      sleep 1
+   
+      echo "-----------------------------------------------------------------------------"
+      echo -e "\n\e[42mThe node has been successfully updated! The current version is $CUR_VER\e[0m\n"
+      echo -e "You can check the operation of farmer with the command: \n\e[31mscreen -r subFarm\e[0m\n"
+      echo "-----------------------------------------------------------------------------"
+      rm latest*
+   fi
    
    #------------------- Блок в случае, если установлена актуальная версия ----------------------------
   
-  else
+   else
       rm latest*
       CUR_VER=${DAEMON_VERSION//subspace-cli-ubuntu-x86_64-/}
       echo -e "\n\e[42mYou have the current ($CUR_VER) version installed!\e[0m\n"
       echo -e "You can check the operation of farmer with the command: \n\e[31mscreen -r subFarm\e[0m\n"
       echo "-----------------------------------------------------------------------------"
-  # fi
+   fi
 fi
