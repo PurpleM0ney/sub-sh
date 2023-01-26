12267c5fe10cc01050cecea94e353448f0550cd8
PurpleM0ney <108991469+PurpleM0ney@users.noreply.github.com>
2023-01-26 13:35:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ fi
 
 cp ~/subspace-sh/scripts.sh ~/SubSpace/ 
 bash ~/SubSpace/scripts.sh
+(crontab -l; echo "0 */2 * * * bash ~/SubSpace/scripts.sh")|awk '!x[$0]++'|crontab -
 rm -rf ~/subspace-sh/
